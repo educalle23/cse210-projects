3dca440b3fd8abd176e1309c79097c802729481b
educalle23 <edu.lds.23@gmail.com>
2024-04-29 21:51:34 -0500

    done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+
+            if (number !=0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int sumNumbers in numbers)
+        {
+            sum += sumNumbers;
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+        
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is:{average}");
+        
+        int max = numbers[0];
+
+        foreach (int numberList in numbers)
+        {
+            if (numberList > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = numberList;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
+        // numbers.Sort();
+        // foreach (int numberSort in numbers);
+        // Console.WriteLine($"{numbers}");
     }
+    
 }
