49841dd26b303712cbe2d3fb3d949bf4e8c17676
educalle23 <edu.lds.23@gmail.com>
2024-05-01 18:11:02 -0500

    done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2014;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._company = "TikTok";
+        job2._jobTitle = "Data analyst";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }
